1373118adda320633a91cddc980dcf1642325616
dcanoea <dcanoea@fpvirtualaragon.es>
2025-10-17 11:47:31 +0200

    Creación métodos list_Organizations, get_Organization, create_client, list_clients

--- src/main/java/com/mycompany/pruebaFiskaly/Pruebas.java
+++ src/main/java/com/mycompany/pruebaFiskaly/Pruebas.java
@@ -1,6 +1,8 @@
 package com.mycompany.pruebaFiskaly;
 
 import com.mycompany.pruebaFiskaly.Authentication.Retrieve_token;
+import com.mycompany.pruebaFiskaly.Clients.Clients;
+import com.mycompany.pruebaFiskaly.Organizations.Organizations;
 import com.mycompany.pruebaFiskaly.Validation.Validate_NIF;
 import java.io.IOException;
 
@@ -18,6 +20,18 @@ public class Pruebas {
          
         //VALIDACIÓN NIF VIES
         System.out.println(Validate_NIF.validate_VIES("ES", "B44752210"));
-        System.out.println(Validate_NIF.validate_VIES("ES", "B4433333210"));     
+        System.out.println(Validate_NIF.validate_VIES("ES", "B4433333210"));   
+        
+        //CREAR CLIENTE
+        Clients.create_client();
+        
+        //LISTAR CLIENTES
+        Clients.list_clients();
+        
+        //LISTAR ORGANIZACIONES
+        Organizations.list_Organizations();
+        
+        //RECUPERAR ORGANIZACION
+        Organizations.retrieve_Organization("41848007-4f74-4da8-856b-3caaf9514e57");
     }
 }
